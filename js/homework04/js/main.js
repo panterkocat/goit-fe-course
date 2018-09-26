72d133ec004ccd2345b6c0f7d49a2cd8b0093914
panterkocat <panterko@icloud.com>
2018-09-26 03:51:21 +0300

    update

---
+++ js/homework04/js/main.js
@@ -0,0 +1,81 @@
+'use strict'
+const products = {
+    bread: 10,
+    milk: 15,
+    apples: 20,
+    chicken: 50,
+    cheese: 40
+};
+
+const order = {
+    bread: 2,
+    milk: 2,
+    apples: 1,
+    cheese: 1
+};
+
+const Cashier = function (name = 'Cashier', productDatabase) {
+    this.name = name;
+    this.productDatabase = productDatabase;
+    this.customerMoney = 0;
+
+    this.getCustomerMoney = function (value) {
+        return this.customerMoney += value;
+    };
+    this.countTotalPrice = function (order) {
+        let sum = 0;
+        let total = 0;
+        for (const key in order) {
+            sum = order[key] *= productDatabase[key]
+            total = sum += total;
+        }
+        return total;
+    };
+    this.countChange = function (totalPrice) {
+        let summ = 0;
+        let cMoney = Number(this.customerMoney);
+        let tPrice = Number(totalPrice);
+        if (cMoney > tPrice) {
+            summ = cMoney - tPrice;
+            return summ;
+        };
+        if (cMoney < tPrice) {
+            summ = cMoney - tPrice;
+            return null;
+        };
+    };
+    this.onSuccess = function (change) {
+        return `Спасибо за покупку, ваша сдача ${change}!`;
+    };
+    this.onError = function () {
+        return `Очень жаль, вам не хватает денег на покупки`;
+    };
+    this.reset = function () {
+        return this.customerMoney = 0 + ` сумма денег обнулена`;
+    };
+};
+
+const mango = new Cashier('Mango', products);
+const totalPrice = mango.countTotalPrice(order);
+
+console.log(mango.name); // Mango
+console.log(mango.productDatabase); // ссылка на базу данных продуктов (объект products)
+console.log(mango.customerMoney); // 0
+console.log(totalPrice); // 110
+mango.getCustomerMoney(300);
+console.log(mango.customerMoney); // 300
+const change = mango.countChange(totalPrice);
+console.log(change); // 190
+const success = mango.onSuccess(change);
+// console.log(success);
+const error = mango.onError();
+// console.log(error);
+const reset = mango.reset();
+// console.log(reset);
+if (change !== null) {
+    // При успешном обслуживании вызываем метод onSuccess
+    console.log(success); // Спасибо за покупку, ваша сдача 190
+} else {
+    // При неудачном обслуживании вызываем метод onError
+    console.log(error); // Очень жаль, вам не хватает денег на покупки
+}
